7fcaecd9be1e8b59cce458724cd6ef49be405009
Cappuccilo <calloy@163.com>
2020-08-13 12:57:36 +0800

    first commit

---
+++ botmanage/set_block.py
@@ -0,0 +1,30 @@
+import hoshino
+import re
+from datetime import timedelta
+from hoshino import Service, priv
+from hoshino.typing import CQEvent
+
+sv = Service('_set_block_', visible=False)
+
+@sv.on_prefix('拉黑')
+async def set_block(bot, ev: CQEvent):
+    if ev.user_id not in bot.config.SUPERUSERS:
+        return
+    for m in ev.message:
+        if m.type == 'at' and m.data['qq'] != 'all':
+            kw = '12小时'
+            time = { '小时': 12, '分钟': 0, '天': 0 }
+            for msg in ev.message:
+                if msg.type == 'text':
+                    kw = msg.data['text'].replace(' ', '')
+                    t_match = re.search(r'(?P<num>[0-9]+)(?P<tp>小时|天|分|分钟)', kw)
+                    if t_match and int(t_match.group('num')) > 0:
+                        time = { '小时': 0, '分钟': 0, '天': 0 }
+                        t = t_match.group('tp')
+                        t = t if t != '分' else '分钟'
+                        time[t] = int(t_match.group('num'))
+                    break
+            uid = int(m.data['qq'])
+            hoshino.priv.set_block_user(uid,timedelta(days=time['天'],minutes=time['分钟'],hours=time['小时']))
+            await bot.send(ev, f"已拉黑{kw}")
+            return
